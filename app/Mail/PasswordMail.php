5ec5c7b947b13ebf4b27dd4ed0f46634b3f05a5c
Mohamed Ashraf <mohammed.ashraaf.ma@gmail.com>
2020-10-18 19:25:14 +0200

    Send password through mail

---
+++ app/Mail/PasswordMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $address = 'info@test.com';
+        $subject = 'Welcome';
+        $name = 'John Doe';
+
+        return $this->view('emails.users.password')
+                    ->from($address, $name)
+                    ->subject($subject)
+                    ->with([ 'data' => $this->data ]);
+    }
+}
---
+++ resources/views/emails/users/password.blade.php
@@ -0,0 +1,10 @@
+<DOCTYPE html>
+    <html lang=”en-US”>
+    <head>
+    <meta charset=”utf-8">
+    </head>
+    <body>
+    <h2>Welcome</h2>
+   <p>Hi {{$data['name']}}, we’re glad you’re here! Following are your password is: {{ $data['password'] }}</p>
+   </body>
+   </html>
